645eeb2d1c11271f05159c534f7f7e6f902b2aee
MostafaAnas <mostafahelal120@gmail.com>
2021-01-11 13:56:47 +0200

    version 1

--- api_client_test.py
+++ api_client_test.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
-def test_face_match():
+def test_face_match(image_path, person_name):
     url = 'http://127.0.0.1:5001/face_rec'
     # open file in binary mode
-    binary_image = {'file': open('images/biden2.jpg', 'rb')}
-    person_info  = {'person_name': 'biden'}     
+    binary_image = {'file': open(image_path , 'rb')}
+    person_info  = {'person_name': person_name}     
     resp = requests.post(url, files=binary_image , data=person_info )
     print( 'face_match response:\n', json.dumps(resp.json()) )
 
+
 if __name__ == '__main__':
-    test_face_match()
+    test_face_match('images/trudeau.jpg','Trudeau')
